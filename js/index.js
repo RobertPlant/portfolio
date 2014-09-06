ed4c506ef172dbcf61518413f95891aee92153ac
rob <trebotplant@gmail.com>
2014-09-06 12:32:27 +0100

    Update javascript and removed unused code

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 function sendRequest() {
-    var length = $('form').serializeArray().length,
-        element = null;
-    var message = "";
+    var form = $('form'),
+        length = form.serializeArray().length,
+        element = null,
+        message = "";
+
     for (var i = 0; i < length; i++) {
-        element = $('form').serializeArray()[i];
+        element = form.serializeArray()[i];
         message += element.value + ' '
     }
-    $("#messageAlert").show()
+    $("#messageAlert").show();
     $.getJSON('https://www.notifymyandroid.com/publicapi/notify?apikey=a8a667d2da1bbf10a9e146eb1e411bc5ec98479c40aa734f&application=Portfolio%20Notification&event=New%20Message&description=' + message + '&callback=?');
 }
